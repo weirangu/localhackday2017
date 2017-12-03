b959153b97f406465a6d27f21d3d96581a8fe5e8
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2017-12-02 20:19:35 -0500

    Load item details from the database

--- app/src/main/java/com/example/localhackday2017/ItemDetailFragment.java
+++ app/src/main/java/com/example/localhackday2017/ItemDetailFragment.java
@@ -3,16 +3,28 @@ package com.example.localhackday2017;
 import android.app.Activity;
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.util.List;
 import java.util.Locale;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * A fragment representing a single Item detail screen.
@@ -60,12 +72,18 @@ public class ItemDetailFragment extends Fragment {
             //View currentView = getView();
             //((TextView)currentView.findViewById(R.id.user_name)).setText(data.userEmail);
         }
+        new GrabOneEventTask().execute(mID);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.item_detail, container, false);
+        repopulate(rootView);
+        return rootView;
+    }
+
+    private void repopulate(View rootView) {
         Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
         String closest = null;
         try {
@@ -82,6 +100,53 @@ public class ItemDetailFragment extends Fragment {
             text += data.eventTags.get(i) + ", ";
         }
         ((TextView) rootView.findViewById(R.id.item_detail)).setText(text);
-        return rootView;
+    }
+    private void refreshData() {
+        repopulate(getView());
+        Activity activity = this.getActivity();
+        CollapsingToolbarLayout appBarLayout = activity.findViewById(R.id.toolbar_layout);
+        if (appBarLayout != null){
+            appBarLayout.setTitle(data.name);
+        }
+
+    }
+    private class GrabOneEventTask extends AsyncTask<Integer, Void, List<EventData>> {
+        @Override
+        protected List<EventData> doInBackground(Integer... myInt) {
+            String postEndpoint = "https://data.chrysalis21.hasura-app.io/v1/query";
+            //postEndpoint = "http://localhost:1234";
+            String jsonStr = null;
+            try {
+                JSONObject whereClause = new JSONObject("{'eventid':{'$eq': " + myInt[0] + "}}");
+                JSONObject queryObject = new JSONObject(EventData.queryTemplate);
+                queryObject.getJSONObject("args").put("where", whereClause);
+                jsonStr = queryObject.toString();
+            } catch (JSONException jsonE) {
+                throw new RuntimeException(jsonE);
+            }
+            String authToken = SecretToken.database;
+            Request request = new Request.Builder()
+                    .url(postEndpoint)
+                    .post(RequestBody.create(MediaType.parse("application/json"), jsonStr))
+                    .addHeader("Authorization", authToken)
+                    .build();
+            OkHttpClient client = new OkHttpClient();
+            try {
+                Response response = client.newCall(request).execute();
+                String body = response.body().string();
+                Log.d("ItemListActivity", body);
+                return EventData.fromJSONString(body);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+        @Override
+        protected void onPostExecute(List<EventData> result) {
+            if (result != null && result.size() == 1) {
+                data = result.get(0);
+                refreshData();
+            }
+        }
     }
 }
